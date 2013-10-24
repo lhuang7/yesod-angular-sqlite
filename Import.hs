b8a3bd05883b93309fecfd9aa84d9d305dbd9384
Scott Murphy <scottmurphy09@gmail.com>
2013-10-24 15:55:57 -0500

    Trying to get rid of a file I shouldn't have comitted

---
+++ Import.hs
@@ -0,0 +1,34 @@
+module Import
+    ( module Import
+    ) where
+
+import           Prelude              as Import hiding (head, init, last,
+                                                 readFile, tail, writeFile)
+import           Yesod                as Import hiding (Route (..))
+
+import           Control.Applicative  as Import (pure, (<$>), (<*>))
+import           Data.Text            as Import (Text)
+
+import           Foundation           as Import
+import           Model                as Import
+import           Settings             as Import
+import           Settings.Development as Import
+import           Settings.StaticFiles as Import
+
+-- | Yesod-Angular Client Route specific 
+import           ClientHandler        as Import
+import           ClientRoutes         as Import
+import           Controllers          as Import 
+
+#if __GLASGOW_HASKELL__ >= 704
+import           Data.Monoid          as Import
+                                                 (Monoid (mappend, mempty, mconcat),
+                                                 (<>))
+#else
+import           Data.Monoid          as Import
+                                                 (Monoid (mappend, mempty, mconcat))
+
+infixr 5 <>
+(<>) :: Monoid m => m -> m -> m
+(<>) = mappend
+#endif
